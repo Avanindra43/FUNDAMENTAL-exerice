a10e05fa6253f47be2b6e7bf77e1986963dabda7
Avanindra43 <irfanavanindra@gmail.com>
2023-11-08 13:59:46 +0300

    Update Exercise.js

--- Exercise.js
+++ Exercise.js
@@ -87,6 +87,167 @@ console.log(titleToNumber("AB"));
 // }
 // console.log(single(arr));
 
+
+class Produk {
+    constructor(menu, price){
+        this.menu = menu;
+        this.price = price;
+    }
+}
+
+class Resto {
+    constructor(nama, jarak){
+        this.nama = nama;
+        this.jarak = jarak;
+    }
+}
+
+class Transaction {
+    #total = 0;
+    #menu = [];
+
+    addToCart(menu, qty, jarak) {
+        if(menu instanceof Produk && qty > 0) {
+            menu.qty = qty;
+            this.#menu.push(menu);
+
+        }else console.log("invalid prdoduct");
+    }
+
+    showTotal() {
+        this.#total = this.#menu.reduce(
+            (sum, current) => sum + current?.price * current?.qty, 0
+        );
+        return this.#total;
+    }
+
+    checkout(pay = 0) {
+        this.showTotal();
+        if (this.#total > pay) return console.log("insuficient balance");
+        this.#menu = [];
+        console.log("Terima kasih sudah berbelanja", "kembalian anda adalah Rp." + 
+        (pay - this.#total.toLocaleString("id-ID"))
+        );
+        return (this.#total = 0)
+    }
+
+    removeFromCart(index) {
+        if (index >= 0 && index < this.#menu.length) {
+            this.#menu.splice(index, 1);
+            console.log("Item removed from the cart");
+        } else {
+            console.log("Invalid index. Item not removed from the cart.");
+        }
+    }
+
+}
+
+const transaction = new Transaction();
+transaction.addToCart(new Produk("capcay", 50000), 2);
+console.log(transaction.showTotal());
+console.log(transaction.checkout(600000));
+
+class Product {
+    constructor(name, stock, category, color) {
+        this.name = name;
+        this.stock = stock;
+        this.category = category;
+        this.color = color;
+    }
+    checkStock() {
+        console.log(`Stock untuk ${this.name}: ${this.stock}`);
+    }
+}
+
+class Tokopedia {
+    constructor() {
+        this.products = []
+        this.cart = []
+    }
+
+    addProduct(name, stock, category, color, extraInfo) {
+        const product = new Product(name, stock, category, color, extraInfo)
+        this.products.push(product);
+    }
+
+    editProduct(name, newStock) {
+        const product = this.products.find((item) => item.name === name);
+        if (product) {
+            product.stock = newStock;
+        } else {
+            console.log("Product not found ;(");
+        }
+    }
+
+    deleteProduct(name) {
+        const index = this.products.findIndex((item) => item.name === name);
+        if (index !== -1) {
+            this.products.splice(index, 1);
+        } else {
+            console.log("Product not found");
+        }
+    }
+
+    addToCart(name, quantity) {
+        const product = this.products.find((item) => item.name === name);
+        if (product && product.stock >= quantity) {
+            product.stock -= quantity;
+            const cartItem = {...product, quantity};
+            this.cart.push(cartItem);
+        } else {
+            console.log("Prduct not found or insufficient stock");
+        }
+    }
+
+    checkout() {
+        for (const cartItem of this.cart) {
+            const product = this.products.find((item) => item.name === cartItem.name);
+            if (product) {
+                product.stock += cartItem.quantity;
+            }
+        }  
+        this.cart = [];
+        console.log("Ckeckout successful. Cart is empty.");
+    }
+
+    checkProductStock(name) {
+        const product = this.products.find((item) => item.name === name)
+        if (product) {
+            product.checkStock();
+        } else {
+            console.log("Product not found");
+        }
+    }
+
+    showCart() {
+        if (this.cart.length === 0) {
+            console.log("Cart is empty.");
+        } else {
+            console.log("Cart:");
+            this.cart.forEach((cartItem) => {
+                console.log(
+                    `${cartItem.name} - Quantity: ${cartItem.quantity} - Category: ${cartItem.category} - Color: ${cartItem.color}`
+                );
+            });
+        }
+    }
+    
+}
+
+const tokopedia = new Tokopedia();
+tokopedia.addProduct("cargo pant", 50, "trousers", "camo", {ukuran: "M", brand: "WATAPS"});
+tokopedia.addProduct("coding for dummies", 50, "buku", "kuning", {author: "dev", brand: "dev"});
+tokopedia.addProduct("Ozweego", 40, "shoes", "grey", {ukuran: "43", brand: "Adidas"});
+tokopedia.addToCart("Ozweego", 5)
+tokopedia.addToCart("cargo pant", 3);
+tokopedia.addToCart("coding for dummies", 1);
+tokopedia.showCart();
+tokopedia.editProduct("coding for dummies", 7)
+tokopedia.showCart()
+tokopedia.checkout()
+tokopedia.showCart()
+tokopedia.checkProductStock("coding for dummies")
+
 function single(array) {
     let res = ''; 
 
